07f654cff36f5c5eecaac1fc9f42eaa0c86e280a
serina113 <s.yanakawa113@outlook.jp>
2024-10-21 06:55:18 +0900

    07-4

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,13 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
-    public function index(post $post)
+    public function index(Post $post)
     {
-        return $post->get();
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
+        // getPaginateByLimit()はPost.phpで定義したメソッドです。
+    }
+    
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post' => $post]);
+    }
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Post $post, PostRequest $request)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
     }
 }
 ?>
